4c37a0081c62f7b2ab78c4787db6402c1b223025
Сурен Давтян <surendavtan@DavtyanS.local>
2023-03-21 23:43:51 +0300

    if

---
+++ exam_if/Program.cs
@@ -0,0 +1,13 @@
+﻿System.Console.WriteLine("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "maria")
+{
+    System.Console.WriteLine("Eeee. Maria here!");
+}
+else
+{
+System.Console.Write("Hi, ");
+System.Console.WriteLine(username);
+
+}
